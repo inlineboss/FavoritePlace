01827211d8a0cfda4c8713608dcbd8aa32cf0961
inlineboss <askimarhond@gmail.com>
2019-12-03 21:05:36 +0300

    Initial Commit

---
+++ FavoritePlace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FavoritePlace
+//
+//  Created by inlineboss on 03.12.2019.
+//  Copyright © 2019 inlineboss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
